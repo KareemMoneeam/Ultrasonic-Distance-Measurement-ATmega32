132d609e9e45e8bcbbe670b0df7de678adfd37e3
Kareem Abd El-Moneam <kareemmoneeam@outlook.com>
2023-10-21 18:06:12 +0300

    Adding Project Files

---
+++ Distance_Measurement_System/Distance_Measurement_System/Debug/Distance_Measurement.d
@@ -0,0 +1,10 @@
+Distance_Measurement.o Distance_Measurement.o: ../Distance_Measurement.c \
+  ../std_types.h ../ultrasonic.h ../gpio.h ../lcd.h
+
+../std_types.h:
+
+../ultrasonic.h:
+
+../gpio.h:
+
+../lcd.h:
